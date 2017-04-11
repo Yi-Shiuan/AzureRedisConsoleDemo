a790d3d2a14137eae20521aa8e53df63058960f7
yi-shiuan <amour99520@gmail.com>
2017-04-11 22:59:40 +0800

    加入專案檔案。

---
+++ AzureRedisConsoleDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using StackExchange.Redis;
+using System;
+
+namespace AzureRedisConsoleDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var redis = ConnectionMultiplexer.Connect("localhost:6379, defaultdatabase=0");
+
+            redis.GetSubscriber().Subscribe("AzureRedisPub", (channel, value) =>
+            {
+                Console.WriteLine($"{DateTime.Now:yyyy/MM/dd HH:mm:ss} - {value}");
+            });
+
+            while (true)
+            {
+                Console.ReadLine();
+            }            
+        }
+    }
+}
